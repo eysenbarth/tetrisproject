2299463a4137ed5727909a0f94064071624571fe
Daniel <glitschy93@gmx.de>
2018-12-11 17:41:34 +0100

    Baseline für Highscore Liste

--- WebContent/Tetris/tetris.js
+++ WebContent/Tetris/tetris.js
@@ -1,31 +1,15 @@
-var audio = document.getElementById("TetrisMelodie");
-		var play = 0;
-		var namestr = "";
-		var score = "";
+var play = 0; 
 
-		function audioScript() {
-			if (play != 0) {
-				audio.pause();
-				play = 0;
-				document.getElementById("mbutton").innerHTML = "Musik abspielen";
-			} else {
-				audio.play();
-				play = 1;
-				document.getElementById("mbutton").innerHTML = "Musik pausieren"
-			}
-		}
-		
-		function getHighscore(){
-			//var x = document.getElementById("namefield");
-			//var y = document.getElementById("testscore");
-			namestr = namefield.elements[0].value;
-			score = testscore.elements[0].value;
-		}
-		function savescore(){
-			getHighscore();
-			if (typeof(Storage) !== "undefined") {
-			localStorage.setItem("high",JSON.stringify({name: namestr, highscore:score}));
-			} else {
-				alert("Sorry, your browser does not support Web Storage...");
-			}
-		}
+function audioScript() { 
+	if (play != 0) { 
+		TetrisMelodie.pause(); 
+		play = 0; 
+		document.getElementById("mbutton").innerHTML = "Musik abspielen"; 
+	} else { 
+		TetrisMelodie.play(); 
+		play = 1; 
+		document.getElementById("mbutton").innerHTML = "Musik pausieren" 
+	} 
+} 
+		 
+	
