951bf0c12416f8ff52f095ef17b7124059cb3112
Daniel Boger <da.boger@ostfalia.de>
2018-12-18 18:39:02 +0100

    Tabellen wird sortiert + refactoring

--- WebContent/Highscore/highscore.js
+++ WebContent/Highscore/highscore.js
@@ -4,15 +4,16 @@ var count;
 var chart = new Array();
 var i;
 
-function get(){
+function showHighscore(){
 	//hier ein String
-	count = localStorage.getItem("count");
-	for(i = 0;i <= Number(count);i++){
-		chart[i] = JSON.parse(localStorage.getItem(JSON.stringify(i))).name + " " +  JSON.parse(localStorage.getItem(JSON.stringify(i))).highscore;
-	}
+	//count = localStorage.getItem("count");
+//	for(i = 0;i <= Number(count);i++){
+//		chart[i] = JSON.parse(localStorage.getItem(JSON.stringify(i))).name + " " +  JSON.parse(localStorage.getItem(JSON.stringify(i))).highscore;
+//	}
 	//result.innerHTML = JSON.parse(localStorage.getItem(count)).name + " " +  JSON.parse(localStorage.getItem(count)).highscore;
-	result.innerHTML = chart;
+	//result.innerHTML = chart;
 	buildTable();
+	sortTable();
 }	
 
 function getHighscore(){
@@ -29,14 +30,14 @@ function setcount(){
 		count = 0;
 	} else{
 		count+=1;	
-	}	
-	alert(count);
+	}
+
+	localStorage.setItem("count", count);
 }
 
 function savescore(){
 	getHighscore();
 	setcount();
-	localStorage.setItem("count", count);
 	if (typeof(Storage) !== "undefined") {
 		//umständlich, da String benötigt
 		localStorage.setItem(localStorage.getItem("count"),JSON.stringify({name: namestr, highscore:score}));
@@ -46,27 +47,63 @@ function savescore(){
 }
 
 function buildTable(){
-	 var table = document.createElement('TABLE');
-	 table.border = '1';
+	var table = document.createElement("TABLE");
+	table.border = 1;
+	table.id = "dynTable"
 
-	 var tableBody = document.createElement('TBODY');
-	 table.appendChild(tableBody);
+	var tableBody = document.createElement("TBODY");
+	table.appendChild(tableBody);
 
-	 for (var i = 0; i <= Number(count); i++) {
-	   var tr = document.createElement('TR');
-	   tableBody.appendChild(tr);
+	for (var i = 0; i <= Number(localStorage.getItem("count")); i++) {
+	  var tr = document.createElement("TR");
+	  tableBody.appendChild(tr);
 
-	   for (var j = 0; j < 2; j++) {
-	     var td = document.createElement('TD');
-	     td.width = '75';
-	     if(j == 0){
-	    	 td.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(JSON.stringify(i))).name));
-	    	 tr.appendChild(td);
-	     }else{
-	    	 td.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(JSON.stringify(i))).highscore));
-	    	 tr.appendChild(td);
-	     }
-	   }
-	 }
-	 hscrTable.appendChild(table);
+	  for (var j = 0; j < 2; j++) {
+	    var td = document.createElement("TD");
+	    td.width = 75;
+	    if(j == 0){
+	    	td.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(JSON.stringify(i))).name));
+	    	tr.appendChild(td);
+	    }else{
+	    	td.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(JSON.stringify(i))).highscore));
+	    	tr.appendChild(td);
+	    }
+	  }
+	}
+	hscrTable.appendChild(table);
+}
+
+function sortTable() {
+	var rows, switching, x, y, shouldSwitch;
+	switching = true;
+	/*Make a loop that will continue until no switching has been done:*/
+	while (switching) {
+		//start by saying: no switching is done:
+		switching = false;
+		rows = dynTable.rows;
+		/*Loop through all table rows (except the first, which contains table headers):*/
+		for (i = 0; i < (rows.length - 1); i++) {
+			//start by saying there should be no switching:
+			shouldSwitch = false;
+			/*Get the two elements you want to compare, one from current row and one from the next:*/
+			x = rows[i].getElementsByTagName("TD")[1];
+			y = rows[i + 1].getElementsByTagName("TD")[1];
+			//check if the two rows should switch place:
+			if (Number(x.innerHTML) < Number(y.innerHTML)) {
+				//if so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
+			}
+		}
+		if (shouldSwitch) {
+			/*If a switch has been marked, make the switch and mark that a switch has been done:*/
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+		}
+	}
+}
+
+function clearStorage(){
+	//debug Function, use with caution!
+	localStorage.clear();
 }
