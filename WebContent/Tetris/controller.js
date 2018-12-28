e9f197bed15cfa088732da4c025281c21855ae9d
eysenbarth <k.keller2106@gmail.com>
2018-12-28 23:00:06 +0100

    wasd-controls hinzugefuegt

--- WebContent/Tetris/controller.js
+++ WebContent/Tetris/controller.js
@@ -1,9 +1,13 @@
 document.body.onkeydown = function( taste ) {
     var tasten = {
         37: 'links',
+        65: 'links',
         39: 'rechts',
+        68: 'rechts',
         40: 'hinab',
-        38: 'drehen'
+        83: 'hinab',
+        38: 'drehen',
+        87: 'drehen'
     };
     if ( typeof tasten[ taste.keyCode ] != 'undefined' ) {
         keyPress( tasten[ taste.keyCode ] );
